97277d0b96e8a966b7c86c8d1ebaa2768a081a9b
Poonna Pipatpanukul <poonna11@gmail.com>
2022-11-22 19:31:21 +0700

    Updates

---
+++ precache-manifest.37cc34432622d73730006c690cc0a9c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6759afdf67ac890128294a3f4189d450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e8c43286e61bd5230b9",
+    "url": "/static/js/2.cb99f82d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cb99f82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815f2d9cb76118f642aa",
+    "url": "/static/js/main.985da87b.chunk.js"
+  },
+  {
+    "revision": "07bcb6c40bae7e96a2bf",
+    "url": "/static/js/runtime-main.fbac11d2.js"
+  }
+]);
